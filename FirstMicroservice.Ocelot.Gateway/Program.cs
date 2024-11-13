d1cd8eff77032c0c86d8cc35c00a4903bd17ee98
alicansariboga <alicnn38@gmail.com>
2024-11-13 20:23:39 +0300

    Ocelot Program.cs

--- FirstMicroservice.Ocelot.Gateway/Program.cs
+++ FirstMicroservice.Ocelot.Gateway/Program.cs
@@ -3,12 +3,16 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
 // OCELOT
 builder.Configuration.AddJsonFile("ocelot.json");
 
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.MapGet("/", () => "Hello World!");
 
